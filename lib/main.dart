871fabc81df0889d7c95c84bfcdf36712a3a5c1b
= <=>
2021-02-10 12:22:16 -0800

    Tugas Praktikum 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
+
+void main() {
+  runApp(MaterialApp(
+      title: "Aplikasi Flutter",
       home: Scaffold(
         appBar: AppBar(
           title: Text('Tugas Pertama Flutter'),
         ),
-        body: Center(
-          child: Container(
-            width: 190,
-            child: Text('Deo Fahmawan 1931710126',textAlign: TextAlign.center,),
+        body: Material(
+          color: Colors.white,
+          child: Center(
+            child: Text(
+              "Deo Fahmawan \n"
+              "1931710126",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.deepPurple, fontSize: 20.0),
+            ),
           ),
         ),
-      ),
-    );
-  }
+      )));
 }
